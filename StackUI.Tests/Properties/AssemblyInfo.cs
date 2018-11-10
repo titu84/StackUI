cd0f6a43375f241addb0d2b541e53e7cdda22ef6
titu84 <krzysztof.sauermann@gmail.com>
2018-11-10 17:09:27 +0100

    init

---
+++ StackUI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StackUI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StackUI.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a807a8e-f715-4a0c-a899-ef5a7e3380c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StackUI.Tests/StackTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace StackUI.Tests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Push_IsExeption()
+        {
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+        }
+        [Test]
+        public void Push_IsAdded()
+        {
+            var stack = new Stack<string>();
+            stack.Push("coś nie string");
+            Assert.That(() => stack.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Pop_IsExeption()
+        {
+            var stack = new Stack<string>();            
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+        [Test]
+        public void Pop_GetsResultAndDeleteFromList()
+        {
+            var stack = new Stack<string>();
+            stack.Push("5");
+            stack.Push("6");
+            stack.Push("7");
+            Assert.That(() => stack.Pop(), Is.EqualTo("7"));
+            Assert.That(() => stack.Count, Is.EqualTo(2));
+        }
+        //Peek
+
+        [Test]
+        public void Peek_IsExeption()
+        {
+            var stack = new Stack<string>();
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+        }
+        [Test]
+        public void Peek_GetsResultAndDeleteFromList()
+        {
+            var stack = new Stack<string>();
+            stack.Push("5");
+            stack.Push("6");
+            stack.Push("7");
+            Assert.That(() => stack.Peek(), Is.EqualTo("7"));
+            Assert.That(() => stack.Peek(), Is.EqualTo("7"));
+            Assert.That(() => stack.Count, Is.EqualTo(3));
+        }
+    }
+}
